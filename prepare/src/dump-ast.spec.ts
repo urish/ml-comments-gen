1ea24c7556384773a5e3b667cc9d4dc7da93b8b5
Uri Shaked <uri@urishaked.com>
2019-07-21 17:22:57 +0300

    refactor(prepare): change dumped AST format
    it no longer includes any comments

--- prepare/src/dump-ast.spec.ts
+++ prepare/src/dump-ast.spec.ts
@@ -2,82 +2,20 @@ import { dumpAst } from './dump-ast';
 
 describe('dumpAst', () => {
   it('should dump AST for a simple expression', () => {
-    expect(dumpAst('a = 5')).toEqual([
-      'ExpressionStatement BinaryExpression Identifier',
-      'ExpressionStatement BinaryExpression FirstAssignment',
-      'ExpressionStatement BinaryExpression FirstLiteralToken',
-      'EndOfFileToken',
-    ]);
+    expect(dumpAst('a = 5')).toEqual(
+      '( ExpressionStatement ( BinaryExpression ( Identifier FirstAssignment FirstLiteralToken ) ) ) EndOfFileToken',
+    );
   });
 
   it('should dump AST for block expression', () => {
-    expect(dumpAst(`for (x = 0; x < 10; x++) { console.log(x) }`)).toEqual([
-      'ForStatement ForKeyword',
-      'ForStatement OpenParenToken',
-      'ForStatement BinaryExpression Identifier',
-      'ForStatement BinaryExpression FirstAssignment',
-      'ForStatement BinaryExpression FirstLiteralToken',
-      'ForStatement SemicolonToken',
-      'ForStatement BinaryExpression Identifier',
-      'ForStatement BinaryExpression FirstBinaryOperator',
-      'ForStatement BinaryExpression FirstLiteralToken',
-      'ForStatement SemicolonToken',
-      'ForStatement PostfixUnaryExpression Identifier',
-      'ForStatement PostfixUnaryExpression PlusPlusToken',
-      'ForStatement CloseParenToken',
-      'ForStatement Block FirstPunctuation',
-      'ForStatement Block ExpressionStatement CallExpression PropertyAccessExpression Identifier',
-      'ForStatement Block ExpressionStatement CallExpression PropertyAccessExpression DotToken',
-      'ForStatement Block ExpressionStatement CallExpression PropertyAccessExpression Identifier',
-      'ForStatement Block ExpressionStatement CallExpression OpenParenToken',
-      'ForStatement Block ExpressionStatement CallExpression Identifier',
-      'ForStatement Block ExpressionStatement CallExpression CloseParenToken',
-      'ForStatement Block CloseBraceToken',
-      'EndOfFileToken',
-    ]);
-  });
-
-  it('should include comment nodes in the AST', () => {
-    expect(dumpAst('a = 5 /* magic */;', true)).toEqual([
-      'ExpressionStatement BinaryExpression Identifier',
-      'ExpressionStatement BinaryExpression FirstAssignment',
-      'ExpressionStatement BinaryExpression FirstLiteralToken',
-      '/* magic */',
-      'ExpressionStatement SemicolonToken',
-      'EndOfFileToken',
-    ]);
-  });
-
-  it('should support multiple comments', () => {
-    expect(dumpAst('/*start*/a// two\n = /*five*/5 /* magic */ /* more magic */;//EOF', true)).toEqual([
-      '/*start*/',
-      'ExpressionStatement BinaryExpression Identifier',
-      '// two',
-      'ExpressionStatement BinaryExpression FirstAssignment',
-      '/*five*/',
-      'ExpressionStatement BinaryExpression FirstLiteralToken',
-      '/* magic */',
-      '/* more magic */',
-      'ExpressionStatement SemicolonToken',
-      '//EOF',
-      'EndOfFileToken',
-    ]);
-  });
-
-  it('should support functions with docstrings', () => {
-    expect(dumpAst('/**\n * test\n */ function f() { return 5; }', true)).toEqual([
-      '/**\n * test\n */',
-      'FunctionDeclaration FunctionKeyword',
-      'FunctionDeclaration Identifier',
-      'FunctionDeclaration OpenParenToken',
-      'FunctionDeclaration SyntaxList',
-      'FunctionDeclaration CloseParenToken',
-      'FunctionDeclaration Block FirstPunctuation',
-      'FunctionDeclaration Block ReturnStatement ReturnKeyword',
-      'FunctionDeclaration Block ReturnStatement FirstLiteralToken',
-      'FunctionDeclaration Block ReturnStatement SemicolonToken',
-      'FunctionDeclaration Block CloseBraceToken',
-      'EndOfFileToken',
-    ]);
+    expect(dumpAst(`for (x = 0; x < 10; x++) { console.log(x) }`)).toEqual(
+      '( ForStatement ( ForKeyword OpenParenToken BinaryExpression ( Identifier FirstAssignment FirstLiteralToken ) ' +
+        'SemicolonToken BinaryExpression ( Identifier FirstBinaryOperator FirstLiteralToken ) ' +
+        'SemicolonToken PostfixUnaryExpression ( Identifier PlusPlusToken ) CloseParenToken ' +
+        'Block ( FirstPunctuation SyntaxList ( ExpressionStatement ( ' +
+        'CallExpression ( PropertyAccessExpression ( Identifier DotToken Identifier ) OpenParenToken ' +
+        'SyntaxList ( Identifier ) CloseParenToken ) ) ) CloseBraceToken ) ) ) ' +
+        'EndOfFileToken',
+    );
   });
 });
--- prepare/src/dump-ast.ts
+++ prepare/src/dump-ast.ts
@@ -1,40 +1,23 @@
 import 'array-flat-polyfill';
-import {
-  createSourceFile,
-  getLeadingCommentRanges,
-  getTrailingCommentRanges,
-  isSourceFile,
-  Node,
-  ScriptTarget,
-  SyntaxKind,
-} from 'typescript';
+import { createSourceFile, Node, ScriptTarget, SyntaxKind } from 'typescript';
 
-function traverse(node: Node, includeComments: boolean): string[] {
+function traverse(node: Node, includeComments: boolean): string {
   const nodeKind = SyntaxKind[node.kind];
-  const source = node.getSourceFile().getFullText();
-  const leadingComments = getLeadingCommentRanges(source, node.pos) || [];
-  const trailingComments = getTrailingCommentRanges(source, node.pos) || [];
-  const allComments = [...leadingComments, ...trailingComments].map((range) =>
-    source.substr(range.pos, range.end - range.pos),
+  if (!node.getChildren().length) {
+    return nodeKind;
+  }
+  return (
+    nodeKind +
+    ' ( ' +
+    node
+      .getChildren()
+      .map((child) => traverse(child, includeComments))
+      .join(' ') +
+    ' )'
   );
-  if (node.kind === SyntaxKind.JSDocComment) {
-    return [];
-  }
-  if (!node.getChildCount()) {
-    return [...allComments, nodeKind];
-  }
-  const prefix = !isSourceFile(node) && node.kind != SyntaxKind.SyntaxList ? nodeKind + ' ' : '';
-  return node
-    .getChildren()
-    .flatMap((child) => traverse(child, includeComments))
-    .map((kindName) => (kindName[0] === '/' ? kindName : prefix + kindName));
-}
-
-function removeDuplicates(items: string[]) {
-  return items.reduce((acc, item) => (acc[acc.length - 1] !== item ? [...acc, item] : acc), []);
 }
 
 export function dumpAst(source: string, includeComments = false) {
   const ast = createSourceFile('', source, ScriptTarget.Latest, true);
-  return removeDuplicates(traverse(ast, includeComments));
+  return traverse(ast, includeComments).replace(/^SourceFile \( SyntaxList | \)$/g, '');
 }
