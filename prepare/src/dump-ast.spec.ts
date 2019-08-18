69a2059e0914cdf34eee01226e3f3093724717e7
Uri Shaked <uri@urishaked.com>
2019-08-18 14:28:38 +0300

    fix(prepare): dump AST correctly
    some token names where dumped incorrectly, e.g. `FirstPunctuation`
    instead of `OpenBraceToken`

--- prepare/src/dump-ast.spec.ts
+++ prepare/src/dump-ast.spec.ts
@@ -3,16 +3,16 @@ import { dumpAst } from './dump-ast';
 describe('dumpAst', () => {
   it('should dump AST for a simple expression', () => {
     expect(dumpAst('a = 5')).toEqual(
-      '( ExpressionStatement ( BinaryExpression ( Identifier FirstAssignment FirstLiteralToken ) ) ) EndOfFileToken',
+      '( ExpressionStatement ( BinaryExpression ( Identifier EqualsToken NumericLiteral ) ) ) EndOfFileToken',
     );
   });
 
   it('should dump AST for block expression', () => {
     expect(dumpAst(`for (x = 0; x < 10; x++) { console.log(x) }`)).toEqual(
-      '( ForStatement ( ForKeyword OpenParenToken BinaryExpression ( Identifier FirstAssignment FirstLiteralToken ) ' +
-        'SemicolonToken BinaryExpression ( Identifier FirstBinaryOperator FirstLiteralToken ) ' +
+      '( ForStatement ( ForKeyword OpenParenToken BinaryExpression ( Identifier EqualsToken NumericLiteral ) ' +
+        'SemicolonToken BinaryExpression ( Identifier LessThanToken NumericLiteral ) ' +
         'SemicolonToken PostfixUnaryExpression ( Identifier PlusPlusToken ) CloseParenToken ' +
-        'Block ( FirstPunctuation SyntaxList ( ExpressionStatement ( ' +
+        'Block ( OpenBraceToken SyntaxList ( ExpressionStatement ( ' +
         'CallExpression ( PropertyAccessExpression ( Identifier DotToken Identifier ) OpenParenToken ' +
         'SyntaxList ( Identifier ) CloseParenToken ) ) ) CloseBraceToken ) ) ) ' +
         'EndOfFileToken',
--- prepare/src/dump-ast.ts
+++ prepare/src/dump-ast.ts
@@ -1,8 +1,9 @@
 import 'array-flat-polyfill';
 import { createSourceFile, Node, ScriptTarget, SyntaxKind } from 'typescript';
+import { syntaxKindMap } from './syntax-kind';
 
 function traverse(node: Node, includeComments: boolean): string {
-  const nodeKind = SyntaxKind[node.kind];
+  const nodeKind = syntaxKindMap[node.kind];
   if (!node.getChildren().length) {
     return nodeKind;
   }
