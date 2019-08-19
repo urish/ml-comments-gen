a882c6b6e64265b202f73632aa829da36cd56d0e
Uri Shaked <uri@urishaked.com>
2019-08-19 17:08:11 +0300

    feat(prepare): replace function name with placeholder

--- prepare/src/rename-args-in-comments.spec.ts
+++ prepare/src/rename-args-in-comments.spec.ts
@@ -2,18 +2,26 @@ import { renameArgsInComments } from './rename-args-in-comments';
 
 describe('renameArgsInComments', () => {
   it('should rename args found in the given comments', () => {
-    const src = `function(a, b) => a + b`;
+    const src = `function(a, b) { return a + b; }`;
     expect(renameArgsInComments('/* sum(a,b): should return the sum of a and b */', src)).toBe(
       '/* sum(ArgumentNumber0,ArgumentNumber1): should return the sum of ArgumentNumber0 and ArgumentNumber1 */',
     );
   });
 
   it('should also rename args for methods', () => {
-    const src = `methodName(foo, bar) => foo * bar`;
+    const src = `methodName(foo, bar) { return foo * bar }`;
     expect(renameArgsInComments('/* multiplies foo by bar */', src)).toBe(
       '/* multiplies ArgumentNumber0 by ArgumentNumber1 */',
     );
   });
+
+  it('replace the function/method name with a placeholder', () => {
+    const src = `takeFive() { return 5 }`;
+    expect(renameArgsInComments('/* the best takeFive implementation */', src)).toBe(
+      '/* the best FunctionNamePlaceholder implementation */',
+    );
+  });
+
 
   it('should not fail for methods with no arguments', () => {
     const src = `methodName() => foo * bar`;
--- prepare/src/rename-args-in-comments.ts
+++ prepare/src/rename-args-in-comments.ts
@@ -3,9 +3,14 @@ import { functionOrMethodAST } from './ast-utils';
 export function renameArgsInComments(comments: string, source: string) {
   const targetNode = functionOrMethodAST(source);
   const argNames = targetNode.parameters.map((p) => p.name.getText());
+  const functionName = targetNode.name && targetNode.name.getText();
   const tokens = comments.match(/\W+|\w+/g) || [];
   return tokens
     .map((token) => {
+      if (functionName && token === functionName) {
+        return 'FunctionNamePlaceholder';
+      }
+
       const index = argNames.indexOf(token);
       return index >= 0 ? `ArgumentNumber${index}` : token;
     })
